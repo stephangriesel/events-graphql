48c97866be63ae274ff68cd8c30766c0ff9cf67f
stephangriesel <sgriesel@gmail.com>
2019-04-02 07:22:06 +0200

    add schema & test

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const bodypParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql')
 
 const app = express();
 
 app.use(bodypParser.json());
 
-app.get('/', (req,res,next) => {
-    res.send('hello');
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Coding', 'Going to movies', 'Swimming'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
