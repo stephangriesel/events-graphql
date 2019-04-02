bb2b967cc71d82bd958e72ecedb937394c091cc3
Steve <sgriesel@gmail.com>
2019-04-02 18:46:39 +0200

    add types & data

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodypParser.json());
 
 // graphql one endpoint where all requests are sent
@@ -14,12 +16,28 @@ app.use(bodypParser.json());
 // mutations change data (crud)
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(` 
+
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
         schema { 
             query: RootQuery
@@ -28,11 +46,17 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return ['Coding', 'Going to movies', 'Swimming'];
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name
-            return eventName;
+            const event = {
+            _id: Math.random().toString(),
+            title: args.title,
+            description: args.description,
+            price: +args.description, // The + operator returns the numeric representation of the object. The + and - operators also have unary versions, where they operate only on one variable. When used in this fashion, + returns the number representation of the object, while - returns its negative counterpart.
+            date: new Date().toISOString()
+        };
+        events.push(event);
         }
     },
     graphiql: true
