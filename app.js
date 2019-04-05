b01e8fa3ea5cb8184959f24783c8613a0948ea0d
Steve <sgriesel@gmail.com>
2019-04-05 14:22:38 +0200

    fetching & posting to db working

--- app.js
+++ app.js
@@ -50,7 +50,15 @@ app.use(
     `),
         rootValue: {
             events: () => {
-                return events;
+                return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return { ...event._doc, _id: event.id}; // keep note of shorter way defined here, longer query event._doc._id.toString()
+                    })
+                })
+                .catch(err => {
+                    throw err;
+                })
             },
             createEvent: args => {
                 const event = new Event({
@@ -63,7 +71,7 @@ app.use(
                 .save()
                 .then(result => {
                     console.log(result);
-                    return{...result._doc};
+                    return{...result._doc, _id: result._doc._id.toString()};
                 })
                 .catch(err => {
                     console.log(err);
