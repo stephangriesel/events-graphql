138b35afa917624faddc6b6b5606a4dbe9ab1b15
Steve <sgriesel@gmail.com>
2019-04-08 06:32:57 +0200

    populate events & users

--- app.js
+++ app.js
@@ -29,12 +29,14 @@ app.use(
             description: String!
             price: Float!
             date: String!
+            creator: User!
         }
 
         type User {
             _id: ID!
             email: String!
             password: String
+            createdEvents: [Event!]
         }
 
         input EventInput {
@@ -64,10 +66,19 @@ app.use(
     `),
         rootValue: {
             events: () => {
-                return Event.find()
+                return Event
+                .find()
+                .populate('creator')
                     .then(events => {
                         return events.map(event => {
-                            return { ...event._doc, _id: event.id }; // keep note of shorter way defined here, longer query event._doc._id.toString()
+                            return { 
+                                ...event._doc, 
+                                _id: event.id, // keep note of shorter way defined here, longer query event._doc._id.toString()
+                            creator: {
+                                ...event._doc.creator._doc,
+                                _id: event._doc.creator.id
+                            }
+                            }; 
                         })
                     })
                     .catch(err => {
