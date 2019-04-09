2e1cab370509d1420ed455073a07aae254ce2f2c
Steve <sgriesel@gmail.com>
2019-04-09 09:07:51 +0200

    update schema

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -3,6 +3,14 @@ const { buildSchema } = require('graphql');
 module.exports = buildSchema(
     ` 
 
+        type Booking {
+            _id: ID!
+            event: Event!
+            user: User!
+            createdAt: String!
+            updatedAt: String!
+        }
+
         type Event {
             _id: ID!
             title: String!
