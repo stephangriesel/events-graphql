1e8fc9b60de586ab6791e35cdafbeb2bca9d2229
Steve <sgriesel@gmail.com>
2019-04-09 11:11:26 +0200

    add cancelbooking

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -149,4 +149,18 @@ module.exports = {
       updatedAt: new Date(result._doc.updatedAt).toISOString()
     };
   },
+  cancelBooking: async args => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate('event');
+      const event = {
+        ...booking.event._doc,
+        _id: booking.event.id,
+        creator: user.bind(this, booking.event._doc.creator)
+      };
+      await Booking.deleteOne({ _id: args.bookingId });
+      return event;
+    } catch (err) {
+      throw err;
+    }
+  }
 };
