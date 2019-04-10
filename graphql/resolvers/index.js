f2c4c9ff39f0f8f47797961a19cd956e80b13da9
Steve <sgriesel@gmail.com>
2019-04-10 15:14:40 +0200

    more refactoring

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -12,7 +12,18 @@ const transformEvent = event => {
     date: dateToString(event._doc.date),
     creator: user.bind(this, event.creator)
   }
-}
+};
+
+const transformBooking = booking => {
+  return {
+    ...booking._doc,
+    _id: booking.id,
+    user: user.bind(this, booking._doc.user),
+    event: singleEvent.bind(this, booking._doc.event),
+    createdAt: dateToString(booking._doc.createdAt),
+    updatedAt: dateToString(booking._doc.updatedAt)
+  }
+};
 
 const events = async eventIds => { // change to async: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
   try {
@@ -62,14 +73,7 @@ module.exports = {
     try {
       const bookings = await Booking.find();
       return bookings.map(booking => {
-        return {
-          ...booking._doc,
-          _id: booking.id,
-          user: user.bind(this, booking._doc.user),
-          event: singleEvent.bind(this, booking._doc.event),
-          createdAt: dateToString(booking._doc.createdAt),
-          updatedAt: dateToString(booking._doc.updatedAt)
-        };
+        return transformBooking(booking);
       });
     } catch (err) {
       throw err;
@@ -129,14 +133,7 @@ module.exports = {
       event: fetchedEvent
     });
     const result = await booking.save();
-    return {
-      ...result._doc,
-      _id: result.id,
-      user: user.bind(this, booking._doc.user),
-      event: singleEvent.bind(this, booking._doc.event),
-      createdAt: dateToString(result._doc.createdAt),
-      updatedAt: dateToString(result._doc.updatedAt)
-    };
+    return transformBooking(result);
   },
   cancelBooking: async args => {
     try {
