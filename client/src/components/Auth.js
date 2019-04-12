14de3623ac05b3161f147b6850f46905d62461e5
Steve <sgriesel@gmail.com>
2019-04-12 11:06:43 +0200

    update nav styles - show active page

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 
 class AuthComponent extends Component {
     render() {
-        return(
-            <h1>Authorized page</h1>
+        return (
+            <form>
+                <div className="form-control">
+                    <label for="email">E-Mail</label>
+                    <input type="email" id="submit" />
+                </div>
+                <div className="form-control">
+                    <label for="password">Password</label>
+                    <input type="password" id="submit" placeholder="***************"/>
+                </div>
+                <div className="form-actions">
+                <button type="button">Switch To Signup</button>
+                <button type="submit">Take Me With</button>
+                </div>
+            </form>
         );
     }
 }
