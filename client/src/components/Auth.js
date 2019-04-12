be23e58eba6fec7b0d094efc18b4633001f1999e
Steve <sgriesel@gmail.com>
2019-04-12 12:42:44 +0200

    add comments - before starting frontend logic

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import './css/Auth.css';
 
 class AuthComponent extends Component {
-    constructor(props) {
+    constructor(props) { 
         super(props);
-        this.emailElement = React.createRef();
+        this.emailElement = React.createRef(); // can also use bindings, this method is for ref's. connect ref's to dom elements in return. 
         this.passwordElement = React.createRef();
     }
-    submitHandler = (event) => {
+    submitHandler = (event) => { // method to send values to backend
         event.preventDefault();
         const email = this.emailElement.current.value;
         const password = this.passwordElement.current.value;
@@ -21,10 +21,10 @@ class AuthComponent extends Component {
         return (
             <form className="form-wrapper" onSubmit={this.submitHandler}>
                 <div className="form-control">
-                    <input type="email" id="email" placeholder="please enter your email" ref={this.emailElement}/>
+                    <input type="email" id="email" placeholder="please enter your email" ref={this.emailElement}/> {/* connect ref's as explained above*/}
                 </div>
                 <div className="form-control">
-                    <input type="password" id="submit" placeholder="***************" ref={this.passwordElement} />
+                    <input type="password" id="submit" placeholder="***************" ref={this.passwordElement} />{/* connect ref's as explained above*/}
                 </div>
                 <div className="form-actions">
                     <button type="submit"><i className="far fa-hand-spock"></i> Take Me With</button>
