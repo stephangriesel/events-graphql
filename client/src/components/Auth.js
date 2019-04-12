f9cf3ea572cb62c1de8673dded5a456ed2bff808
Steve <sgriesel@gmail.com>
2019-04-12 12:22:26 +0200

    test frontend submission - success

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react';
 import './css/Auth.css';
 
 class AuthComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.emailElement = React.createRef();
+        this.passwordElement = React.createRef();
+    }
+    submitHandler = (event) => {
+        event.preventDefault();
+        const email = this.emailElement.current.value;
+        const password = this.passwordElement.current.value;
+        if (email.trim().length === 0 || password.trim().length === 0){
+            return;
+        }
+        console.log(email,password);
+        // send request to backend
+    }
     render() {
         return (
-            <form className="form-wrapper">
+            <form className="form-wrapper" onSubmit={this.submitHandler}>
                 <div className="form-control">
-                    <label for="email"></label>
-                    <input type="email" id="submit" placeholder="please enter your email" />
+                    <input type="email" id="email" placeholder="please enter your email" ref={this.emailElement}/>
                 </div>
                 <div className="form-control">
-                    <label for="password"></label>
-                    <input type="password" id="submit" placeholder="***************" />
+                    <input type="password" id="submit" placeholder="***************" ref={this.passwordElement} />
                 </div>
                 <div className="form-actions">
-                    <button type="submit"><i class="far fa-hand-spock"></i> Take Me With</button>
+                    <button type="submit"><i className="far fa-hand-spock"></i> Take Me With</button>
 
-                    <button type="button">Sign Me Up <i class="far fa-check-circle"></i></button>
+                    <button type="button">Sign Me Up <i className="far fa-check-circle"></i></button>
                 </div>
             </form>
         );
