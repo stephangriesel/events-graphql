bc9db1d22db4843a2f9fbd678b528ab736587726
Steve <sgriesel@gmail.com>
2019-04-16 15:22:37 +0200

    update auth component

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -2,29 +2,57 @@ import React, { Component } from 'react';
 import './css/Auth.css';
 
 class AuthComponent extends Component {
-    constructor(props) { 
+    state = {
+        isLoggedIn: true
+    };
+
+    constructor(props) {
         super(props);
         this.emailElement = React.createRef(); // 1. can also use bindings, this method is for ref's. connect ref's to dom elements in return. 
         this.passwordElement = React.createRef();
     }
+
+    switchModeHandler = () => {
+        this.setState(prevState => {
+            return { isLogin: !prevState.isLoggedIn };
+        })
+
+    }
+
     submitHandler = (event) => { // method to send values to backend
         event.preventDefault(); // default behaviour to send request to same address this is running, which is not correct, preventDefault prevents this
         const email = this.emailElement.current.value; // 3. get values from what is defined in render (the form)
         const password = this.passwordElement.current.value;
-        if (email.trim().length === 0 || password.trim().length === 0){ // validation, trim whitespace
+        if (email.trim().length === 0 || password.trim().length === 0) { // validation, trim whitespace
             return;
         }
 
-        const requestBody = {
+        let requestBody = {
             query: `
-                mutation {
-                    createUser(userInput: {email:"${email}", password:"${password}"}) {
-                        _id
-                        email
-                    }
+            query {
+                login(email: "${email}", password:"${password}") {
+                    userId
+                    token
+                    tokenExpiration
                 }
+            }
             `
         };
+
+        if (!this.state.isLoggedIn) {
+            requestBody = {
+                query: `
+                    mutation {
+                        createUser(userInput: {email:"${email}", password:"${password}"}) {
+                            _id
+                            email
+                        }
+                    }
+                `
+            };
+        }
+
+
         // console.log(email,password); // test if working
         // send request to backend
         fetch('http://localhost:8000/graphql', { // not using axios. fetch built into modern browsers
@@ -33,21 +61,33 @@ class AuthComponent extends Component {
             headers: {
                 'Content-Type': 'application/json'
             }
-        });
+        }).then(res => {
+            if (res.status !== 200 && res.status !== 201) {
+                throw new Error('Failed!');
+            }
+            return res.json();
+        })
+            .then(resData => {
+                console.log(resData);
+            })
+            .catch(err => {
+                console.log(err); // show error messages, not status codes, more like network issues
+            })
     };
     render() {
         return (
             <form className="form-wrapper" onSubmit={this.submitHandler}> {/* listen to form submit, this will always refer to class because arrow function used above */}
                 <div className="form-control">
-                    <input type="email" id="email" placeholder="please enter your email" ref={this.emailElement}/> {/* 2. connect ref's as explained above*/}
+                    <input type="email" id="email" placeholder="please enter your email" ref={this.emailElement} /> {/* 2. connect ref's as explained above*/}
                 </div>
                 <div className="form-control">
                     <input type="password" id="submit" placeholder="***************" ref={this.passwordElement} />{/* connect ref's as explained above*/}
                 </div>
                 <div className="form-actions">
-                    <button type="submit"><i className="far fa-hand-spock"></i> Take Me With</button>
-
-                    <button type="button">Sign Me Up <i className="far fa-check-circle"></i></button>
+                    <button type="submit">Submit</button>
+                    <button type="button" onClick={this.switchModeHandler}>
+                        Take Me To {this.state.isLogin ? 'Signup Page' : 'Your Leader'} <i className="far fa-hand-spock"></i>
+                    </button>
                 </div>
             </form>
         );
