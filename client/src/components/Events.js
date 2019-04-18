4f12d0d0d04b6733b7864e1db153abaab01124e8
Steve <sgriesel@gmail.com>
2019-04-18 09:25:55 +0200

    start working on events page

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import './css/Events.css';
+
 
 class EventsComponent extends Component {
     render() {
-        return(
-            <h1>Events page</h1>
+        return (
+            <div className="events-wrapper">
+                <h1>Ready to book?</h1>
+                <button>Add Booking</button>
+            </div>
         );
     }
 }
