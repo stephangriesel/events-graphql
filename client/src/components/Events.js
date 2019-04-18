63704d26e15d8aa1f6854526389cf52c3be23202
Steve <sgriesel@gmail.com>
2019-04-18 11:07:48 +0200

    add logic to open modal

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -5,15 +5,25 @@ import Backdrop from './Backdrop';
 
 
 class EventsComponent extends Component {
+    state = {
+        creating: false
+    }
+
+    startCreateEventHandler = () => {
+        this.setState({ creating: true }) // condition to display or hide backdrop
+    }
     render() {
         return (
             <React.Fragment>
-            <Backdrop />
-            <Modal title="Add Booking" canCancel canConfirm><h1>Nice</h1></Modal> {/* canCancel & canConfirm will be set as true */}
-            <div className="events-wrapper">
-                <h1>Ready to book?</h1>
-                <button>Add Booking</button>
-            </div>
+                {this.state.creating && <Backdrop />}
+                {this.state.creating &&
+                    <Modal title="Add Booking" canCancel canConfirm>
+                        <h1>Nice</h1>
+                    </Modal>} {/* canCancel & canConfirm will be set as true */}
+                <div className="events-wrapper">
+                    <h1>Ready to book?</h1>
+                    <button onClick={this.startCreateEventHandler}>Add Booking</button>
+                </div>
             </React.Fragment>
         );
     }
