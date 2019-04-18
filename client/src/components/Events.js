926b8e3aa4881444bc485571b5509dd943c5803c
Steve <sgriesel@gmail.com>
2019-04-18 17:47:35 +0200

    update logic for confirm handler - test successful

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -9,12 +9,38 @@ class EventsComponent extends Component {
         creating: false
     };
 
+    constructor(props) { // binding was also option
+        super(props);
+        this.titleElementRef = React.createRef();
+        this.priceElementRef = React.createRef();
+        this.dateElementRef = React.createRef();
+        this.descriptionElementRef = React.createRef();
+    }
+
     startCreateEventHandler = () => {
         this.setState({ creating: true }) // condition to display or hide backdrop/modal
     };
 
     modalConfirmHandler = () => {
         this.setState({ creating: false });
+        const title = this.titleElementRef.current.value; // get values entered on frontend
+        const price = +this.priceElementRef.current.value; // the '+' in front of this converts string to number
+        const date = this.dateElementRef.current.value;
+        const description = this.descriptionElementRef.current.value;
+
+        if (
+            title.trim().length === 0 ||
+            price <= 0 ||
+            date.trim().length === 0 ||
+            description.trim().length === 0 
+        ) {
+            return;
+        }
+
+        const event = {title:title,price:price,date:date,description:description};
+        console.log(event); 
+
+        
     };
     modalCancelHandler = () => {
         this.setState({ creating: false });
@@ -34,19 +60,19 @@ class EventsComponent extends Component {
                         <form> {/* compare to schema*/}
                             <div className="form-wrapper__events">
                                 {/* <label htmlFor="title">What kind of booking ye majesty?</label> */}
-                                <input type="text" id="title" placeholder="Booking title ye majesty?"></input>
+                                <input type="text" id="title" placeholder="Booking title ye majesty?" ref={this.titleElementRef}></input>
                             </div>
                             <div className="form-wrapper__events">
                                 {/* <label htmlFor="price">Price</label> */}
-                                <input type="number" id="price" placeholder="Set your price"></input>
+                                <input type="number" id="price" placeholder="Set your price" ref={this.priceElementRef}></input>
                             </div>
                             <div className="form-wrapper__events">
                                 {/* <label htmlFor="date">Date</label> */}
-                                <input type="date" id="date" placeholder="Choose your date"></input>
+                                <input type="datetime-local" id="date" placeholder="Choose your date" ref={this.dateElementRef}></input>
                             </div>
                             <div className="form-wrapper__events">
                                 {/* <label htmlFor="description">Description</label> */}
-                                <textarea id="description" rows="2" />
+                                <textarea id="description" rows="10" ref={this.descriptionElementRef}/>
                                 </div>
                         </form>
                     </Modal>} {/* canCancel & canConfirm will be set as true */}
