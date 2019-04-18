16ec18b2ae94ad9383b567c78bb12be226548cad
Steve <sgriesel@gmail.com>
2019-04-18 10:15:45 +0200

    little buggy modal - to be fixed

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './css/Events.css';
+import Modal from './Modal';
+import Backdrop from './Auth';
+import './css/Backdrop.css';
 
 
 class EventsComponent extends Component {
     render() {
         return (
+            <React.Fragment>
+            <Backdrop />
+            <Modal title="Add Booking" canCancel canConfirm><h1>Nice</h1></Modal> {/* canCancel & canConfirm will be set as true */}
             <div className="events-wrapper">
                 <h1>Ready to book?</h1>
                 <button>Add Booking</button>
             </div>
+            </React.Fragment>
         );
     }
 }
---
+++ client/src/components/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './css/Modal.css';
+
+const modal = props => (
+    <div className="modal">
+        <header class="modal__header">{props.title}Test</header>
+        <section className="modal__content">
+            {props.children} {/*  allow to pass from outside */}
+        </section>
+        <section className="modal__actions">
+            {props.canCancel && <button className="btn">Cancel</button>}
+            {props.canConfirm && <button className="btn">Confirm</button>}
+        </section>
+    </div>
+); // modal receive props and return modal content
+
+export default modal;
