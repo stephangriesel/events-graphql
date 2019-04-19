84afc069586a573f7678bf19684746b592b94d72
Steve <sgriesel@gmail.com>
2019-04-19 15:35:47 +0200

    item styles update

--- client/src/components/EventItem.js
+++ client/src/components/EventItem.js
@@ -4,7 +4,15 @@ import './css/EventItem.css';
 
 const eventItem = props => (
     <li key={props._id} className="event__list-item">
-    {props.title}
+        <div class="item-details">
+            <div class="detailed-item">
+                <h1>{props.title}</h1>
+                <h2>$1.00</h2>
+            </div>
+            <div class="detailed-item">
+                <button>View Details</button>
+            </div>
+        </div>
     </li>
 );
 
