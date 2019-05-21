c99dc61062c1d511140430ffb6c1531b949b53bb
Steve <sgriesel@gmail.com>
2019-05-21 11:31:30 +0200

    add price filters - buggy

--- client/src/components/BookingStats.js
+++ client/src/components/BookingStats.js
@@ -1,6 +1,26 @@
 import React from 'react'
 
-function BookingStats() {
+const BOOKINGS_BUCKETS = {
+  'Low': 100,
+  'Medium': 250,
+  'High': 500
+}
+
+const BookingStats = props => {
+  const output = {};
+  for (const bucket in BOOKINGS_BUCKETS) {
+    const filteredBookingsCount = props.bookings.reduce((
+      prev, current
+    ) => {
+      if (current.price < BOOKINGS_BUCKETS[bucket]) {
+        return prev + 1
+      } else {
+        return prev;
+      }
+    },0);
+    output[bucket] = filteredBookingsCount;
+  }
+  console.log(output);
   return (
     <div>
       stATS
